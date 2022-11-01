9acdf8922517951006ff8e7298911e104dd14b7c
konkeror <sid.mkjee@gmail.com>
2022-11-01 15:46:04 +0100

    Add example to check empty vector.

---
+++ STL/vectors/vector_initialise.cpp
@@ -0,0 +1,14 @@
+// C++ program to illustrate the
+// element access in vector
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	vector<int> g1;
+    if (g1.empty())
+        cout << "here";
+
+	return 0;
+}
+
