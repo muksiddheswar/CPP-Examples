a0c521cfa610a716db11461fbabef1c6861acf86
konkeror <sid.mkjee@gmail.com>
2022-11-09 12:31:21 +0100

    CPPExamples: Add example of pass by reference in class

---
+++ parameter_passing/pass_by_reference_in_class.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MyClass {       // The class
+  public:             // Access specifier
+
+    MyClass() { // Constructor with parameters
+      myNum1 = 1;
+      myNum2 = 100;
+    }
+
+    void change_my_num1()
+    {
+        change_value(& myNum1);
+    }
+
+    void change_my_num2()
+    {
+        change_value(& myNum2);
+    }
+
+    int change_value(int* num )
+    {
+        *num += 1;
+        return *num;
+    }
+
+    void value_print()
+    {
+        cout<< endl<<myNum1;
+        cout<< endl<<myNum2;
+    }
+
+    private:
+        int myNum1;
+        int myNum2;
+
+
+
+};
+
+int main() {
+    MyClass myObj;  // Create an object of MyClass
+    myObj.value_print();
+    myObj.change_my_num1();
+    myObj.value_print();
+  
+    return 0;   
+}
