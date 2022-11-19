e3b4ba0980b8a151a969bb4d0d0490b7fedf725a
konkeror <sid.mkjee@gmail.com>
2022-11-19 18:12:17 +0100

    CPPExamples: Add example string erase

---
+++ STL/strings/string_erase_example.cpp
@@ -0,0 +1,28 @@
+// CPP code to illustrate
+// erase(iterator pos)
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function to demo erase
+void eraseDemo(string str)
+{
+	// Deletes character at position 4
+	str.erase(str.begin() + 4);
+
+	cout << "After erase : ";
+	cout << str;
+}
+
+// Driver code
+int main()
+{
+	string str("Hello World!");
+
+	cout << "Before erase : ";
+	cout << str << endl;
+	eraseDemo(str);
+
+	return 0;
+}
