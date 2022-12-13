837439bf299345b56c6bb58b6203d29fd6c351a9
konkeror <sid.mkjee@gmail.com>
2022-12-13 07:18:26 +0100

    CPPExamples: Add CONST examples

---
+++ declarations_and_definitions/const_value_example.cpp
@@ -0,0 +1,10 @@
+int main() {
+   const int i = 5;
+//    i = 10;   
+//    i++;   
+
+// constant_values2.cpp
+const int maxarray = 255;
+char store_char[maxarray];  // allowed in C++; not allowed in C
+
+}
