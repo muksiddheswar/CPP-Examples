ae6d72a5815bb56a53ead95fd9b8b002828e021b
Siddheswar Mukherjee <smukherjee@omp.com>
2022-12-13 16:03:23 +0100

    CPPExamples: const char* vs string example.

---
+++ type_manipulation/const_char.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    const char* string = "Hello World";
+    const std::string st = "string";
+    // const std::string* st_1 = "string";
+
+    cout << string;
+}
