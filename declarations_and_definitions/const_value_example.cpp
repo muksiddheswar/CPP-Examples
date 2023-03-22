d2b50d7a8d7795253313e19883797bbdf39a3c5d
Siddheswar Mukherjee <smukherjee@omp.com>
2023-03-22 11:13:16 +0100

    CPPExamples: Rearrange CONST examples

--- declarations_and_definitions/const_value_example.cpp
+++ declarations_and_definitions/const_value_example.cpp
@@ -1,10 +1,11 @@
 int main() {
    const int i = 5;
-//    i = 10;   
-//    i++;   
+   //    i = 10;   
+   //    i++;   
 
-// constant_values2.cpp
-const int maxarray = 255;
-char store_char[maxarray];  // allowed in C++; not allowed in C
+   const int maxarray = 255;
+   int x = 10;
+   char store_char[maxarray];  // allowed in C++; not allowed in C
+   char store_char2[x];
 
 }
