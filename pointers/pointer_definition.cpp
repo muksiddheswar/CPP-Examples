3ea09b7fd828b13952fc5696f4f083767bac9d58
Siddheswar Mukherjee <smukherjee@omp.com>
2023-03-23 17:17:38 +0100

    CPPExamples: Add non string pointer definition example.

---
+++ pointers/pointer_definition.cpp
@@ -0,0 +1,16 @@
+// C++ program to illustrate concept
+// of the constant pointers
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+
+	int a = 90;
+    int* ptr = &a;
+    int& ptr1 = a;
+
+
+	return 0;
+}
