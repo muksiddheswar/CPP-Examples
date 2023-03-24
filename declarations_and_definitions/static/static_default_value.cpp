dde44ba09405cb14b4266e3e4cb737a0a8cc4f9b
Siddheswar Mukherjee <smukherjee@omp.com>
2023-03-24 16:27:32 +0100

    CPPExamples: Add example for static default  value.

---
+++ declarations_and_definitions/static/static_default_value.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    static int i;
+    static char b;
+    static float f;
+    cout << i << endl;
+    cout << b << endl;
+    cout << f << endl;
+    return 0;
+}
