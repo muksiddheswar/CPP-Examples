0dda29c10348c0aeeb554746baf052bfc69774d2
konkeror <sid.mkjee@gmail.com>
2023-03-25 19:13:49 +0100

    Read file using dirent functions

--- file_management/read_files_with_dirent.cpp
+++ file_management/read_files_with_dirent.cpp
@@ -1,18 +1,22 @@
+#include<bits/stdc++.h>
 #include <iostream>
 #include <dirent.h>
 #include <sys/types.h>
 using namespace std;
 
 int main(void) {
-   DIR *dr;
-   struct dirent *en;
-   dr = opendir("\"D:\\Codes\\Tiny-CPP-Projects\\File-Management\\test_directory\""); //open all directory
-   if (dr) 
-   {
-      while ((en = readdir(dr)) != NULL) {
-         cout<<" \n"<<en->d_name; //print all directory name
-      }
-      closedir(dr); //close all directory
-   }
-   return(0);
+    DIR *dr;
+    struct dirent *en;
+    vector<string> filenames;
+    vector<string> folders;
+
+    dr = opendir("./test_directory"); //open all directory
+    if (dr) 
+    {
+        while ((en = readdir(dr)) != NULL) {
+            cout<<" \n"<<en->d_name; //print all directory name
+        }
+        closedir(dr); //close all directory
+    }
+return(0);
 }
