ea20e3c5ec682ef725dedaea70af463060f5214e
Siddheswar Mukherjee <smukherjee@omp.com>
2023-05-01 14:18:04 +0200

    Restructured code

--- virtual_function/abstract_derived_class_constructor.cpp
+++ virtual_function/abstract_derived_class_constructor.cpp
@@ -5,18 +5,20 @@ using namespace std;
 class Base
 {
 protected:
-int x;
+	int x;
 public:
-virtual void fun() = 0;
-Base(int i) {
-			x = i;
-			cout<<"Constructor of base called\n";
-			}
+	virtual void fun() = 0;
+	Base(int i) 
+	{
+		x = i;
+		cout<<"Constructor of base called\n";
+	}
 };
 
 class Derived: public Base
 {
 	int y;
+	
 public:
 	Derived(int i, int j):Base(i) { y = j; }
 	void fun() { cout << "x = " << x << ", y = " << y<<'\n'; }
